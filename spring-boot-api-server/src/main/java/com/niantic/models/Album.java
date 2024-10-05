8f7c0a1aaf8ca6086cef1f0b37c4fd1ef7ce9ee7
Jordan Napoleon <jordannagcaoili@gmail.com>
2024-10-05 11:22:08 -0700

    lombok changes

--- spring-boot-api-server/src/main/java/com/niantic/models/Album.java
+++ spring-boot-api-server/src/main/java/com/niantic/models/Album.java
@@ -1,11 +1,16 @@
 package com.niantic.models;
 
-import jakarta.validation.constraints.NotBlank;
-
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Date;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Album
 {
     private int albumId;
@@ -13,64 +18,5 @@ public class Album
     private String title;
     private String description;
     private LocalDateTime createdAt;
-    private ArrayList<Post> posts = new ArrayList<>();
 
-    public Album(){}
-
-    public Album(int albumId, int userId, String title, String description, LocalDateTime createdAt) {
-        this.albumId = albumId;
-        this.userId = userId;
-        this.title = title;
-        this.description = description;
-        this.createdAt = createdAt;
-        this.posts = posts;
-    }
-
-    public int getAlbumId() {
-        return albumId;
-    }
-
-    public void setAlbumId(int albumId) {
-        this.albumId = albumId;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public ArrayList<Post> getPosts() {
-        return posts;
-    }
-
-    public void setPosts(ArrayList<Post> posts) {
-        this.posts = posts;
-    }
 }
