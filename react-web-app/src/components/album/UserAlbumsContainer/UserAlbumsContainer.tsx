2d819d0d9294982cab195c600d359a2dee185820
Eri (Windows) <eri-ishikawa@proton.me>
2024-10-09 11:25:26 -0700

    UI improvements to Albums List

--- react-web-app/src/components/album/UserAlbumsContainer/UserAlbumsContainer.tsx
+++ react-web-app/src/components/album/UserAlbumsContainer/UserAlbumsContainer.tsx
@@ -4,7 +4,7 @@ import "./UserAlbumsContainer.css"
 import { useState, useEffect } from "react";
 import { Album } from "../../../models/album";
 import albumService from "../../../services/album-service";
-import AlbumThumbnailCard from "../../profile/AlbumThumbnailCard";
+import AlbumThumbnailCard from "../AlbumThumbnailCard/AlbumThumbnailCard";
 import AlbumAddModal from "../AlbumAddModal";
 import { Link } from "react-router-dom";
 
@@ -24,7 +24,7 @@ export default function UserAlbumsContainer({profileId}: {profileId: number})
         <section id="album-container" className="mt-5">
             {
                 albums.map((album) => (
-                    <Link to={`/albums/${album.albumId}`}>
+                    <Link to={`/albums/${album.albumId}`} className="link">
                         <AlbumThumbnailCard key={album.albumId}
                         albumId={album.albumId}
                         title={album.title}
