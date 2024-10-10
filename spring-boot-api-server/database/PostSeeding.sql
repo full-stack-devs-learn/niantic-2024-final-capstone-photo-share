0921e3698f2da195ee2cfc140b6570f90d4092e3
Jordan Napoleon <jordannagcaoili@gmail.com>
2024-10-10 10:37:48 -0700

    seed

--- spring-boot-api-server/database/PostSeeding.sql
+++ spring-boot-api-server/database/PostSeeding.sql
@@ -1,16 +1,6 @@
 USE photo_share;
 
 
--- PROFILE SEEDING --
-INSERT INTO profiles(user_id, profile_img, bio)
-VALUES
-(1, "qipoecbc5evjszubti0l", "Hello, I'm user 1!"),
-(2, "profile_img2.jpg", "This is user 2's bio."),
-(3, "profile_img3.jpg", "User 3 here, nice to meet you!"),
-(4, "profile_img4.jpg", "Welcome to user 4's profile!"),
-(5, "profile_img5.jpg", "Hey, I'm user 5!"),
-(6, "Ash_3_rbbtbo", "Hey, I'm Ash Ketchum from Pallet Town");
-
 -- POST SEEDING --
 INSERT INTO posts (user_id, public_id, title, captions, created_at, album_id)
 VALUES
