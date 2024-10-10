901f720886f8699a556fa185582f026eb8d40425
Eri (Windows) <eri-ishikawa@proton.me>
2024-10-10 11:16:37 -0700

    Progress on page num bug

--- react-web-app/src/components/photo-post/PhotoPostFeed/PhotoPostFeed.tsx
+++ react-web-app/src/components/photo-post/PhotoPostFeed/PhotoPostFeed.tsx
@@ -8,11 +8,14 @@ import { PhotoPost } from "../../../models/photo-post";
 import { Button } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store/store";
+import { faCaretRight, faCaretLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function PhotoPostFeed({onNewPostCreated}: {onNewPostCreated: any})
 {
     const { user, isAuthenticated } = useSelector((state: RootState) => state.authentication);
     const [posts, setPosts] = useState<PhotoPost[]>([]);
+    const [currentPosts, setCurrentPosts] = useState<PhotoPost[]>([]);
     const [pageNum, setPageNum] = useState<number>(1);
 
     useEffect(() => {
@@ -26,12 +29,23 @@ export default function PhotoPostFeed({onNewPostCreated}: {onNewPostCreated: any
         {
             photoPostService.getAllPosts(pageNum).then(data => {
                 setPosts(data);
+                if(posts == currentPosts)
+                {
+                    setPageNum(pageNum - 1);
+                }
             })
         }
     }, [onNewPostCreated, pageNum, isAuthenticated]);
-
-    const handleNextPage = () => setPageNum(pageNum + 1);
+    
     const handlePrevPage = () => setPageNum(pageNum - 1);
+    const handleNextPage = () => {
+        setPageNum(pageNum + 1);
+        
+        if(currentPosts !== posts)
+        {
+            setCurrentPosts(posts);
+        }
+    };
 
     return (
         <>
@@ -49,9 +63,11 @@ export default function PhotoPostFeed({onNewPostCreated}: {onNewPostCreated: any
             ))
         }
 
-        {pageNum !== 1 && <Button onClick={handlePrevPage}>Prev</Button>}
+        <div className="d-flex gap-5 justify-content-between align-items-center mb-5">
+            {pageNum !== 1 && <Button onClick={handlePrevPage}><FontAwesomeIcon icon={faCaretLeft} /> Prev</Button>}
 
-        <Button className="mb-5" onClick={handleNextPage}>Next</Button>
+            <Button onClick={handleNextPage}>Next <FontAwesomeIcon icon={faCaretRight} /></Button>
+        </div>
         </>
     )
 }
