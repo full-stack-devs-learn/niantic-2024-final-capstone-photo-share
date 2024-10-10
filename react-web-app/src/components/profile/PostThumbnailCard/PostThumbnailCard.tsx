3f794a2e6f30ac52cf3892bec29f9f01f6bed7d6
Eri (Windows) <eri-ishikawa@proton.me>
2024-10-09 22:57:20 -0700

    user can click on post in profile

--- react-web-app/src/components/profile/PostThumbnailCard/PostThumbnailCard.tsx
+++ react-web-app/src/components/profile/PostThumbnailCard/PostThumbnailCard.tsx
@@ -1,32 +1,13 @@
 import "./PostThumbnailCard.css"
 
-import { useState } from "react";
 import { Cloudinary } from '@cloudinary/url-gen';
 import { AdvancedImage } from '@cloudinary/react';
 import { autoGravity } from '@cloudinary/url-gen/qualifiers/gravity';
 import { auto } from '@cloudinary/url-gen/actions/resize';
-import { Card, Modal } from 'react-bootstrap';
-import { useParams } from "react-router-dom";
+import { Card } from 'react-bootstrap';
 
-export default function ThumbnailCard({title, publicId}: {title: string, publicId: string})
+export default function ThumbnailCard({postId, title, publicId, cardClicked}: {postId: number, title: string, publicId: string, cardClicked: any})
 {
-    const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
-    const params = useParams();
-    const postId = params?.postId ?? 0;
-
-    const showPost = () =>
-    {
-        if(postId !== 0)
-        {
-            setShow(true);
-            console.log(postId);
-        }
-    }
-    
     const cld = new Cloudinary({ cloud: { cloudName: import.meta.env.VITE_CLOUDINARY_CLOUD_NAME } });
 
     const img = cld
@@ -37,14 +18,10 @@ export default function ThumbnailCard({title, publicId}: {title: string, publicI
 
     return (
         <>
-        <Card className="post-card" onClick={handleShow}>
+        <Card className="post-card" onClick={() => cardClicked(postId)}>
             <AdvancedImage className="thumbnail-image" cldImg={img} />
             <Card.Body className="post-card-body">{title}</Card.Body>
         </Card>
-
-        <Modal show={show} onHide={handleClose}>
-        <Modal.Body>Yay</Modal.Body>
-        </Modal>
         </>
     )
 }
--- react-web-app/src/components/profile/UserPostsContainer/UserPostsContainer.tsx
+++ react-web-app/src/components/profile/UserPostsContainer/UserPostsContainer.tsx
@@ -5,10 +5,23 @@ import { useState, useEffect } from "react";
 import { PhotoPost } from "../../../models/photo-post";
 import photoPostService from "../../../services/photo-post-service";
 import PostThumbnailCard from "../PostThumbnailCard/PostThumbnailCard";
+import { Modal } from "react-bootstrap";
+import PhotoPostCard from "../../photo-post/PhotoPostCard";
 
 export default function UserPhotosContainer({profileId}: {profileId: number})
 {
     const [posts, setPosts] = useState<PhotoPost[]>([]);
+    const [selectedPost, setSelectedPost] = useState<PhotoPost>();
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+
+    function handleClick(postId: number)
+    {
+        setShow(true);
+        console.log(postId)
+        setSelectedPost(posts.filter(post => post.postId == postId)[0]);
+    }
 
     useEffect(() => {
         photoPostService.getByUser(profileId).then(data => {
@@ -21,11 +34,28 @@ export default function UserPhotosContainer({profileId}: {profileId: number})
             {
                 posts.map((post) => (
                     <PostThumbnailCard key={post.postId}
+                        postId={post.postId}
                         publicId={post.publicId}
                         title={post.title}
+                        cardClicked={handleClick}
                         ></PostThumbnailCard>
                 ))
             }
+
+        {show == true &&
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Body className="mx-auto">
+                    <PhotoPostCard key={selectedPost!.postId}
+                        userId={selectedPost!.userId}
+                        postId={selectedPost!.postId}
+                        publicId={selectedPost!.publicId}
+                        title={selectedPost!.title}
+                        captions={selectedPost!.captions}
+                        reactions={selectedPost!.reactions}
+                        hasInteracted={selectedPost!.hasInteracted}
+                        ></PhotoPostCard>
+                </Modal.Body>
+            </Modal>}
         </section>
     )
 }
